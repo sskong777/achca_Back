208377a03c8320ddb5c312c70508f69f1bab787e
sskong777 <sskong777@gamil.com>
2022-05-25 17:14:49 +0900

    install django-heroku

--- config/settings.py
+++ config/settings.py
@@ -168,3 +168,8 @@ SITE_ID = 1
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+
+
+import django_heroku
+django_heroku.settings(locals())
